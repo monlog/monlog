29c8e75871dd395c5cf0f740ff75526b2998bf9e
Patrick <patku564@student.liu.se>
2012-01-30 09:33:36 -0800

    No need for db-script since syncdb takes care of it

--- DB_CREATE.sql
+++
@@ -1,7 +0,0 @@
--- HOWTO:
--- mysql -uroot -p < DB_CREATE.sql
-
-CREATE DATABASE monlog;
-USE monlog;
-CREATE USER 'monlog'@'localhost' IDENTIFIED BY 'monlog123';
-GRANT CREATE,ALTER,DELETE,SELECT,UPDATE,INSERT ON monlog.* TO 'monlog';
