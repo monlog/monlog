27adb57c0772587d1e7adc0183e4f4fc7101ae57
Albin Englund <mail@albinenglund.com>
2012-02-02 12:07:23 +0000

    Fixed git conflict data from source

--- monlog/log/models.py
+++ monlog/log/models.py
@@ -2,10 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class LogMessage(models.Model):
-<<<<<<< HEAD
-    user = models.ForeignKey(User)
-=======
->>>>>>> 45807df395c011b138110dee8e594f10eb62b1a9
     date = models.DateTimeField()
     short_desc = models.CharField(max_length=100)
     long_desc = models.TextField()
--- monlog/settings.py
+++ monlog/settings.py
@@ -140,20 +140,11 @@ except ImportError:
 
     DATABASES = {
         'default': {
-<<<<<<< HEAD
             'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': '',                      # Or path to database file if using sqlite3.
             'USER': '',                      # Not used with sqlite3.
             'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-=======
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'monlog',                      # Or path to database file if using sqlite3.
-            'USER': 'monlog',                      # Not used with sqlite3.
-            'PASSWORD': 'monlog123',                  # Not used with sqlite3.
-            'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '3317',                      # Set to empty string for default. Not used with sqlite3.
->>>>>>> 45807df395c011b138110dee8e594f10eb62b1a9
         }
     }
