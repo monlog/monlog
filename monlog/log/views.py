38f07befcdec280ae96d148c38f9c026639eabf2
Albin Englund <mail@albinenglund.com>
2012-02-06 14:17:13 +0000

    Added templates with bootstrap

--- monlog/log/views.py
+++ monlog/log/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Foobar")
+    return render_to_response('index.html', context_instance=RequestContext(request))
--- monlog/settings.py
+++ monlog/settings.py
@@ -80,12 +80,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'monlog.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -149,3 +143,9 @@ except ImportError:
     # in apps' "static/" subdirectories and in STATICFILES_DIRS.
     # Example: "/home/media/media.lawrence.com/static/"
     STATIC_ROOT = ''
+    
+    TEMPLATE_DIRS = (
+        # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+    )
