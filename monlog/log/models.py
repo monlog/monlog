c6e206192fe514b6b86ba89478ee867a47db95d2
Patrick <patku564@student.liu.se>
2012-02-06 06:09:23 -0800

    Made unauthorized test case

--- monlog/log/models.py
+++ monlog/log/models.py
@@ -5,6 +5,7 @@ from tastypie.models import create_api_key
 models.signals.post_save.connect(create_api_key, sender=User)
 
 class LogMessage(models.Model):
+    user = models.ForeignKey(User)
     date = models.DateTimeField()
     short_desc = models.CharField(max_length=100)
     long_desc = models.TextField()
