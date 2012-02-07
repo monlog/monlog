bc9239ac7edc1362de86c19f3a7ad47d857b95d0
Samuel Andersson <smulster@gmail.com>
2012-02-07 10:42:26 +0100

    Added fields to log message model

--- monlog/log/models.py
+++ monlog/log/models.py
@@ -4,9 +4,23 @@ from tastypie.models import create_api_key
 
 models.signals.post_save.connect(create_api_key, sender=User)
 
+SEVERITY_CHOICES = ( 
+                    (0, 'Debug'),
+                    (1, 'Info'),
+                    (2, 'Notify'),
+                    (3, 'Warning'),
+                    (4, 'Error'),
+                    (5, 'Critical'),
+                    (6, 'Alert'),
+                    (7, 'Emergency')
+                   )
+
 class LogMessage(models.Model):
-    user = models.ForeignKey(User)
-    date = models.DateTimeField()
+    """ Model of a log message """
+    severity = models.CharField(max_length=1, choices=SEVERITY_CHOICES)
+    datetime = models.DateTimeField()
+    server_ip = models.GenericIPAddressField(unpack_ipv4=True)
+    application = models.ForeignKey(User)
     short_desc = models.CharField(max_length=100)
     long_desc = models.TextField()
 
