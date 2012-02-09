c959ffd931a84a36a3b09d627281883ab200b2e9
Martin Melin <git@martinmelin.com>
2012-02-09 11:52:49 +0100

    added django extensions, try manage.py shell_plus

--- monlog/settings.py
+++ monlog/settings.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'log',
     'django_filters',
+    'django_extensions',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
