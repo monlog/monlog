e04a57a06728f757ed37a2675380b56c3bad3331
Martin Melin <git@martinmelin.com>
2012-02-09 11:53:07 +0100

    display log messages in list view instead of dummy data

--- monlog/log/views.py
+++ monlog/log/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from models import LogMessage
 from filters import LogMessageFilterSet
 
 @login_required
 def list(request):
     context = RequestContext(request)
-    context['filterset'] = LogMessageFilterSet(request.GET or None)
+    queryset = LogMessage.objects.all()
+    context['filterset'] = LogMessageFilterSet(request.GET or None, queryset=queryset)
     return render_to_response('list.html', context)
