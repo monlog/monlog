f67c9b2f4e896bcd60dfd44b88ecf82aa17b6952
Patrick <patku564@student.liu.se>
2012-02-13 11:16:33 -0800

    Added function which is called on if data is malformed. It posts a 'monlog' log in monlog to notify user

--- monlog/log/validation.py
+++ monlog/log/validation.py
@@ -1,8 +1,34 @@
 
 from tastypie.validation import Validation
-
+from log.models import LogMessage
+from django.contrib.auth.models import User
+import datetime
+import simplejson as json
 
 class LogValidation(Validation):
+    def log_malformed_data(self, data, errors):
+        """
+        If data provided by client is corrupt or malformed, we handle it here
+        by adding an extra log to the system by monlog, notifying the user about
+        the problem.
+        """
+        current_date = datetime.datetime.now()
+        desc = json.dumps(data)
+        monlog_user = User.objects.get(pk=1)
+        severity = 4;
+        server_ip = "127.0.0.1"
+
+        if 'datetime' in errors:
+            # We found malformed datetime
+            log = LogMessage(datetime = current_date, long_desc = desc, short_desc = "Monlog: Discovered malformed datetime in log message.", application = monlog_user, server_ip = server_ip, severity = severity)
+            log.save()
+            
+        elif 'severity' in errors:
+            # We found incorrect severity
+            log = LogMessage(datetime = current_date, long_desc = desc, short_desc = "Monlog: Discovered incorrect severity in log message.", application = monlog_user, server_ip = server_ip, severity = severity)
+            log.save()
+        
+    
     def is_valid(self, bundle, request=None):
         """
         Performs a check on ``bundle.data``to ensure it is valid.
@@ -40,7 +66,9 @@ class LogValidation(Validation):
         else:
             if data['severity'] < 0 or data['severity'] > 7:
                 errors['severity'] = "Not supported severity level."
-     
+        if len(errors) > 0:
+            self.log_malformed_data(data, errors)
+        
         return errors
     
 
