92d2b19ed6cef18f6ee16cf015b5509fb8de6548
Martin Melin <git@martinmelin.com>
2012-02-20 14:55:32 +0100

    get rid of dummy data for application and server choices

--- monlog/log/views.py
+++ monlog/log/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from models import LogMessage, SEVERITY_CHOICES
 from filters import LogMessageFilterSet
+from django.contrib.auth.models import User
 
 @login_required
 def list(request):
     context = RequestContext(request)
     queryset = LogMessage.objects.all()
     context['severity_choices'] = SEVERITY_CHOICES
+    context['application_choices'] = User.objects.all()
+    context['server_choices'] = LogMessage.objects.all().order_by('server_ip').values('server_ip').distinct()
     context['filterset'] = LogMessageFilterSet(request.GET or None, queryset=queryset)
     return render_to_response('list.html', context)
