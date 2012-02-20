879c2af1a94fda42f2f84236967ffcd44f50f54f
Martin Melin <git@martinmelin.com>
2012-02-20 16:59:51 +0100

    polls for new matching log messages every 5 seconds, displays notice if present (but clicking notice doesn't do anything ATM)

--- monlog/log/static/js/api.js
+++ monlog/log/static/js/api.js
@@ -1,4 +1,10 @@
-var requestLogMessages = function() {
+var pendingData;
+var lastDisplayedDatetime;
+// if refresh requests return more than this, we don't know exact number of new messages
+var maxObjects = 20;
+var refreshTimeout = 5000;
+
+var requestLogMessages = function(update) {
     var formData = $.map($('.filters').serializeArray(), function(n) {
         if (n.value == "") {
             return null;
@@ -6,20 +12,41 @@ var requestLogMessages = function() {
         return n;
     });
 
+    if (! update) { // this is a refresh request, so we should only match new log messages
+        if (typeof lastDisplayedDatetime !== 'undefined') {
+            formData.push({ 'name': 'datetime__gte', 'value': lastDisplayedDatetime });
+        }
+    }
+
     var url = "/api/logmessages/?" + $.param(formData);
     $.getJSON(url,
         function(data,textStatus,jqXHR) {
-            $(".content .table tbody").html(ich.log_messages(data));
+            if (update) {
+                $(".content .table tbody").html(ich.log_messages(data));
+                lastDisplayedDatetime = data['objects'][data['objects'].length - 1]['datetime'];
+                $('#refresh_notice').hide();
+            } else {
+                pendingData = data;
+                var count = data['objects'].length;
+                if (count >= maxObjects) {
+                    count = count + "+";
+                }
+                $('#refresh_notice').html(ich.refresh_notice_template({ 'count': count }));
+                $('#refresh_notice').show();
+                window.setTimeout(function() { requestLogMessages(false); }, refreshTimeout);
+            }
         }
     );
 }
 
 $(document).ready(function() {
-    $('.filters input, .filters select').change(requestLogMessages);
-    $('.filters .search-query').keypress(requestLogMessages);
+    var updateHandler = function() { requestLogMessages(true); };
+    $('.filters input, .filters select').change(updateHandler);
+    $('.filters .search-query').keypress(updateHandler);
     $('form.filters').submit(function(event){
-        requestLogMessages();
+        requestLogMessages(true);
         event.preventDefault();
     });
-    requestLogMessages();
+    requestLogMessages(true);
+    window.setTimeout(function() { requestLogMessages(false); }, refreshTimeout);
 });
