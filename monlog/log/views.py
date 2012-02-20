b3e851930838ebd4dd49efa9416eb04fbb20b598
Martin Melin <git@martinmelin.com>
2012-02-20 14:57:02 +0100

    get rid of obsolete django_filter

--- monlog/log/views.py
+++ monlog/log/views.py
@@ -3,7 +3,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from models import LogMessage, SEVERITY_CHOICES
-from filters import LogMessageFilterSet
 from django.contrib.auth.models import User
 
 @login_required
@@ -13,5 +12,4 @@ def list(request):
     context['severity_choices'] = SEVERITY_CHOICES
     context['application_choices'] = User.objects.all()
     context['server_choices'] = LogMessage.objects.all().order_by('server_ip').values('server_ip').distinct()
-    context['filterset'] = LogMessageFilterSet(request.GET or None, queryset=queryset)
     return render_to_response('list.html', context)
