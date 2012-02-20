1f1689a978801c46aa3552c6f04db1b6b4ca630a
Samuel Andersson <smulster@gmail.com>
2012-02-20 13:17:09 +0100

    added view for label

--- monlog/log/views.py
+++ monlog/log/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-from models import LogMessage
+from models import LogMessage, Label
 from filters import LogMessageFilterSet
 
 @login_required
@@ -11,3 +11,11 @@ def list(request):
     queryset = LogMessage.objects.all()
     context['filterset'] = LogMessageFilterSet(request.GET or None, queryset=queryset)
     return render_to_response('list.html', context)
+
+@login_required
+def save_label(request):
+    label = Label(name=request.POST['name'], 
+                  query_string=request.POST['query_string'] )
+    label.save()
+    return render_to_response('label_saved.html')
+    
