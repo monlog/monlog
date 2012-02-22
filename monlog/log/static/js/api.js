633f4a458adb0cb06445197b3fa607da33b23454
Martin Melin <git@martinmelin.com>
2012-02-22 12:00:16 +0100

    new data inserted after a refresh is highlighted by making the row bold

--- monlog/log/static/js/api.js
+++ monlog/log/static/js/api.js
@@ -1,8 +1,9 @@
 var pendingData;
+var displayedIds;
 var lastDisplayedDatetime;
 // if refresh requests return more than this, we don't know exact number of new messages
 var maxObjects = 20;
-var refreshTimeout = 500;
+var refreshTimeout = 5000;
 
 // ISO 8601 date format function from:
 // https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference:Global_Objects:Date#Example:_ISO_8601_formatted_dates
@@ -16,22 +17,38 @@ function ISODateString(d){
       + pad(d.getSeconds())
 }
 
-var displayLogMessages = function(data, replace) {
+var displayLogMessages = function(data) {
     var count = data['objects'].length;
+    var newDisplayedIds = [];
     if (count > 0) {
+        $.each(data['objects'], function(i, el) {
+            if (typeof displayedIds == 'undefined' || $.inArray(el['id'], displayedIds) !== -1) {
+                // this is an old log message
+                el['old'] = true;
+            }
+            newDisplayedIds.push(el['id']);
+        });
+        console.log(data);
         $(".content .table tbody").html(ich.log_messages(data));
     }
     lastDisplayedDatetime = ISODateString(new Date());
+    displayedIds = newDisplayedIds;
     $('#refresh_notice').hide();
 };
 
-var requestLogMessages = function(update) {
+var requestLogMessages = function(update, was_refresh) {
     var formData = $.map($('.filters').serializeArray(), function(n) {
         if (n.value == "") {
             return null;
         }
         return n;
     });
+
+    if (was_refresh) {
+        $('.content table').addClass('was-refreshed');
+    } else if (update) {
+        $('.content table').removeClass('was-refreshed');
+    }
 
     if (! update) { // this is a refresh request, so we should only match new log messages
         if (typeof lastDisplayedDatetime !== 'undefined') {
@@ -45,6 +62,7 @@ var requestLogMessages = function(update) {
     $.getJSON(url,
         function(data,textStatus,jqXHR) {
             if (update) {
+                console.log($('.content table tbody tr'));
                 displayLogMessages(data, true);
                 $("#loading_indicator").fadeOut(300);
             } else {
@@ -64,21 +82,21 @@ var requestLogMessages = function(update) {
 };
 
 var refreshClickHandler = function(event) {
-    requestLogMessages(true);
+    requestLogMessages(true, true);
     $('#refresh_notice').hide();
 };
 
 $(document).ready(function() {
-    var updateHandler = function() { requestLogMessages(true); };
+    var updateHandler = function() { requestLogMessages(true, false); };
     $('.filters input, .filters select').change(updateHandler);
     // disable until we have full text search
     // needs a timeout so it doesn't trigger a search for every key press right away
     // $('.filters .search-query').keypress(updateHandler);
     $('form.filters').submit(function(event){
-        requestLogMessages(true);
+        requestLogMessages(true, false);
         event.preventDefault();
     });
     $('#refresh_notice').bind('click', refreshClickHandler);
-    requestLogMessages(true);
-    window.setTimeout(function() { requestLogMessages(false); }, refreshTimeout);
+    requestLogMessages(true, false);
+    window.setTimeout(function() { requestLogMessages(false, false); }, refreshTimeout);
 });
