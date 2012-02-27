bb2da3a3a5c66c8eda10c63b7715c535324579a7
Martin Melin <git@martinmelin.com>
2012-02-27 15:53:53 +0100

    perform some magic to get a zero-conf wsgi.py

--- monlog/wsgi.py
+++ monlog/wsgi.py
@@ -1,10 +1,10 @@
-import os, site
+import os, sys, site
 
 # parent directory of wsgi.py
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # add the virtual environment
-site.addsitedir(os.path.join(path, 'venv'))
+site.addsitedir(os.path.join(path, 'venv', 'lib', "python%s.%s" % (sys.version_info[0], sys.version_info[1]), 'site-packages'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monlog.settings")
 
