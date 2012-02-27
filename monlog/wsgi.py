bab47e337ce23d2011830768e9905ffe3f26c5ea
Martin Melin <git@martinmelin.com>
2012-02-27 16:40:43 +0100

    make the WSGI script work

--- monlog/wsgi.py
+++ monlog/wsgi.py
@@ -1,12 +1,15 @@
+# This assumes that your virtual environment is located at:
+# /path/to/monlog/venv
 import os, sys, site
 
 # parent directory of wsgi.py
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project = os.path.dirname(os.path.abspath(__file__))
 
 # add the virtual environment
-site.addsitedir(os.path.join(path, 'venv', 'lib', "python%s.%s" % (sys.version_info[0], sys.version_info[1]), 'site-packages'))
+site.addsitedir(os.path.join(parent, 'venv', 'lib', "python%s.%s" % (sys.version_info[0], sys.version_info[1]), 'site-packages'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monlog.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
