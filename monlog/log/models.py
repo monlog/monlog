0cb1a5160d674e8c0d10b130efd48ef2a1cd8487
Samuel Andersson <smulster@gmail.com>
2012-03-21 16:58:23 +0100

    added expectations model with a few associated functions

--- monlog/log/models.py
+++ monlog/log/models.py
@@ -2,8 +2,18 @@ from django.contrib.auth.models import User
 from django.db import models
 from tastypie.models import create_api_key
 from django.http import QueryDict
+from datetime import timedelta, datetime
 
 models.signals.post_save.connect(create_api_key, sender=User)
+
+EXPECTATION_UNITS = (
+                    (0,'month'),
+                    (1,'week'),
+                    (2,'day'),
+                    (3,'hour'),
+                    (4,'minute'),
+                    (5,'second')
+                  )
 
 SEVERITY_CHOICES = (
                     (0, 'debug'),
@@ -70,4 +80,76 @@ class Label(Filter):
 
     def __unicode__(self):
         return self.label_name
+
+
+class Expectation(Filter):
+    """
+    Model for expectations.
+    """
+
+    # timestamp for next deadline
+    deadline = models.DateTimeField()
+
+    # +- amount of unit
+    # example: '+- 10 minute'
+    tolerance_amount = models.IntegerField()
+    tolerance_unit = models.IntegerField()
+
+    # repeat every ``repeat_delta`` ``repeat_unit``
+    # example: 'every 2 month'
+    repeat_delta = models.IntegerField()
+    repeat_unit = models.IntegerField()
+
+    least_amount_of_hits = models.IntegerField()
+
+    def apply_tolerance(self):
+        """
+        Sets startdate and enddate for filter.
+
+        Startdate = Deadline - Tolerance
+        Enddate   = Deadline + Tolerance
+        """
+
+        timedelta = get_timedelta(tolerance_unit, tolerance_amount)
+        startdate = (self.deadline - timedelta).replace(microsecond=0)
+        enddate   = (self.deadline + timedelta).replace(microsecond=0)
+        if !self.query_string.empty():
+            self.query_string += "&"
+        self.query_string += "datetime__gte=" + startdate.isoformat() + "&datetime__lte=" + enddate.isoformat()
+
+    def check_expectation(self):
+        """
+        Checks if X amount of log messages matches the filter.
+        X is the least amount of log messages we need in order to accept the expectation.
+        """
+        return LogMessage.objects.filter(self.get_dict()) >= self.least_amount_of_hits
+
+    def next_deadline(self):
+        """
+        Returns next deadline as a datetime object. Does NOT change the deadline.
+        """
+        timedelta = (get_timedelta(repeat_unit, repeat_delta).replace(microsecond=0))
+        return deadline + timedelta
+
+    def get_timedelta(self, unit, value):
+        """
+        Returns a timedelta object from specified unit and value. See ``EXPECTATION_UNITS`` for valid units.
+        """
+        if value <= 0 return None
+
+        if unit == 0: # month
+            return timedelta(weeks=(4*value))
+        elif unit == 1: # week
+            return timedelta(weeks=value)
+        elif unit == 2: # day
+            return timedelta(days=value)
+        elif unit == 3: # hour
+            return timedelta(hours=value)
+        elif unit == 4: # minute
+            return timedelta(minutes=value)
+        elif unit == 5: # second
+            return timedelta(seconds=value)
+        else:
+            # Invalid unit
+            return None
 
